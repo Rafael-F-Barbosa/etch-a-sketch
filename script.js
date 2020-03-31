80f4b4e407aa9cc01dcf625b7828d925dc258097
Rafael Barbosa <rafaelfbarbosa7@gmail.com>
2020-03-31 14:07:33 -0300

    Possibility of choose how much boxes the user wants added

--- script.js
+++ script.js
@@ -1,48 +1,82 @@
+const buttonCreate = document.querySelector(".createButton");
+buttonCreate.addEventListener('click', changeNumber);
 
 
-const container = document.querySelector("#container")
-
-
-// Creation of the boxes
-let i, j;
-for(i=0; i <4; i++)
-{
-    const newLine = document.createElement('div');
-    newLine.classList.add('lines');
-    container.appendChild(newLine);
-
-    for (let j = 0; j < 4; j++) {
-        const element = document.createElement('div');
-        element.classList.add('element');
-        newLine.appendChild(element);
-    }
-    
-}
-
-function putColor(){
-    this.style.backgroundColor = randomColor()
-}
-
-const boxes = document.querySelectorAll('.element');
-boxes.forEach(key => key.addEventListener('mouseover' , putColor));
-
-function clear()
-{
-    const boxes = document.querySelectorAll('.element');
-    boxes.forEach(key => key.style.backgroundColor = '#AAAAAA');
-}
 const button = document.querySelector('button');
 button.addEventListener('click', clear);
 
-function randomColor()
-{
-    const arrayHex = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+changeNumber();
+
+// apply the random color
+function putColor() {
+    this.style.backgroundColor = randomColor()
+}
+
+// generates a random color
+function randomColor() {
+    const arrayHex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
     let colorString = ""
 
     for (let i = 0; i < 6; i++) {
-        let randomNumber = Math.floor((Math.random()*16))
+        let randomNumber = Math.floor((Math.random() * 16))
         colorString += arrayHex[randomNumber]
     }
 
-    return ("#"+ colorString);
+    return ("#" + colorString);
 }
+
+// Creation of the boxes
+
+function boxCreation(lin, col) {
+    const container = document.querySelector('#container')
+    for (i = 0; i < lin; i++) {
+        let i, j;
+        
+        const newLine = document.createElement('div');
+        newLine.classList.add('lines');
+        container.appendChild(newLine);
+
+        for (let j = 0; j < col; j++) {
+            const element = document.createElement('div');
+            element.setAttribute('id', 'element')
+            newLine.appendChild(element);
+        }
+
+    }
+}
+
+function eraseBoxes(){
+    const container = document.querySelector('#container')
+    while(container.firstChild)
+    {
+        console.log(container);
+        container.removeChild(container.firstChild)
+    }  
+}
+
+// Changing the number of boxes
+function changeNumber() {
+    
+    eraseBoxes()
+
+    const numberOfBoxes = document.querySelector("#numberBoxes")
+    boxCreation(numberOfBoxes.value, numberOfBoxes.value)
+
+    
+
+    // Create the event listener for all of the boxes
+    let boxes = document.querySelectorAll('#element');
+    boxes.forEach(key => key.addEventListener('mouseover', putColor));
+}
+
+// Give the boxes the original color
+function clear() {
+    const boxes = document.querySelectorAll('#element');
+    boxes.forEach(key => key.style.backgroundColor = '#AAAAAA');
+}
+
+
+
+
+
+
